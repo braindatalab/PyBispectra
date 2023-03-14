c6133caffccf545619e24fe3e7c8f86c524b31f5
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-03-14 18:26:12 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,8 @@
 
 import os
 import sys
-import mock
+
+# import mock
 
 # MOCK_MODULES = [
 #    "pybispectra",
@@ -21,6 +22,8 @@ import mock
 # for mod_name in MOCK_MODULES:
 #    sys.modules[mod_name] = mock.Mock()
 
+sys.path.insert(0, os.path.abspath("../.."))
+
 from pybispectra.utils import linkcode_resolve
 
 
@@ -31,8 +34,6 @@ release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-sys.path.insert(0, os.path.abspath("../.."))
 
 extensions = [
     "sphinx.ext.mathjax",
