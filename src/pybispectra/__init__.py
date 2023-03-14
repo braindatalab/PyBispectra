1657a7cba48d852d964390015d1e023bd47f268d
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-03-14 17:49:57 +0100

    bug fix rtd

--- src/pybispectra/__init__.py
+++ src/pybispectra/__init__.py
@@ -1,5 +1,10 @@
 __version__ = "0.0.1"
 
-from pybispectra import cfc
-from pybispectra import tde
-from pybispectra import utils
+from pybispectra.cfc import PAC, PPC
+from pybispectra.tde import TDE
+from pybispectra.utils import (
+    ResultsCFC,
+    ResultsTDE,
+    compute_fft,
+    fast_find_first,
+)
