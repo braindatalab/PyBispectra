d5824e470ef2a0d2a2f059c6ed9b6f100c496308
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-03-14 18:40:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 """Install the pybispectra package."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="pybispectra",
     version="0.0.1",
     package_dir={"": "src/"},
-    packages=find_packages(),
+    packages=[
+        "pybispectra",
+        "pybispectra.cfc",
+        "pybispectra.tde",
+        "pybispectra.utils",
+    ],
 )
