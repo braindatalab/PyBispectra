1745a89dd2378dbb5e791550e510c0d8bac0fd8b
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-03-15 20:38:42 +0100

    add tde tests

--- src/pybispectra/test_pac.py
+++ src/pybispectra/test_pac.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pybispectra.cfc import PAC, PPC
+from pybispectra.tde import TDE
 from pybispectra.utils import compute_fft
 
 if __name__ == "__main__":
@@ -8,15 +9,22 @@ if __name__ == "__main__":
     sfreq = 100
     n_jobs = 1
 
+    data = rand.rand(2, 2, 220)
+    data[:, 1, 20:] = data[:, 0, :200].copy()
+    data = data[:, :, :200]
+
     fft, freqs = compute_fft(data, sfreq, n_jobs)
 
-    ppc = PPC(fft, freqs)
-    ppc.compute(n_jobs=n_jobs)
-    ppc_results = ppc.results
+    # ppc = PPC(fft, freqs)
+    # ppc.compute(n_jobs=n_jobs)
+    # ppc_results = ppc.results
 
     # pac = PAC(fft, freqs)
     # pac.compute(symmetrise="none", normalise="none", n_jobs=n_jobs)
     # pac_results = pac.results
     # pac_results[0].plot()
+
+    tde = TDE(fft, freqs)
+    tde.compute(symmetrise="none", n_jobs=n_jobs)
 
     print("jeff")
