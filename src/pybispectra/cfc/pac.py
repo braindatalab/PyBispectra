bug fix normalisation

src/pybispectra/cfc/pac.py
@@ -292,7 +292,7 @@ class PAC(_ProcessBispectrum):
             )
         )
 
-        self._bicoherence = self._bispectrum / threenorm
+        self._bicoherence = np.abs(self._bispectrum) / threenorm
 
         if self.verbose:
             print("        ... Bicoherence computation finished\n")

src/pybispectra/utils/_process.py
@@ -370,6 +370,6 @@ def _compute_threenorm(
                     (np.abs(fft_f1) ** 3).mean()
                     * (np.abs(fft_f2) ** 3).mean()
                     * (np.abs(fft_fdiff) ** 3).mean()
-                ) ** 1 / 3
+                ) ** (1 / 3)
 
     return results