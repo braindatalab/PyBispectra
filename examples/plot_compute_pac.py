5d75228f4e671da5559ae6ffa22102ed566a670d
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-08-30 08:40:44 +0100

    test rtd

--- examples/plot_compute_pac.py
+++ examples/plot_compute_pac.py
@@ -66,7 +66,7 @@ from pybispectra import compute_fft, PAC
 
 # %%
 
-data_dir = os.path.join(Path(os.getcwd()).parent.parent, "_static", "data")
+data_dir = os.path.join(Path(os.getcwd()).parent, "_static", "data")
 
 # load simulated data
 data = np.load(os.path.join(data_dir, "sim_data_pac_bivariate.npy"))
--- examples/plot_compute_waveshape.py
+++ examples/plot_compute_waveshape.py
@@ -83,7 +83,7 @@ from pybispectra import compute_fft, WaveShape
 
 # %%
 
-data_dir = os.path.join(Path(os.getcwd()).parent.parent, "_static", "data")
+data_dir = os.path.join(Path(os.getcwd()).parent, "_static", "data")
 
 # load example data
 data_sawtooths = np.load(
