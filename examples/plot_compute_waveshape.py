178ba0834f6a7754a5691d80a20f335866174932
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-08-30 11:23:09 +0100

    test rtd

--- examples/plot_compute_waveshape.py
+++ examples/plot_compute_waveshape.py
@@ -83,7 +83,7 @@ from pybispectra import compute_fft, WaveShape
 
 # %%
 
-data_dir = os.path.join(os.path.abspath(".."), "_static", "data")
+data_dir = os.path.join(os.path.abspath(".."), "examples", "data")
 
 # load example data
 data_sawtooths = np.load(
