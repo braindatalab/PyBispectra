b78f8aaf46aeda896122e29e0d6b3aa76298f3b3
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-08-31 15:47:28 +0200

    test rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,29 +49,6 @@ sphinx_gallery_conf = {
 templates_path = ["_templates"]
 exclude_patterns = []
 
-# Get path to directory containing this file, conf.py.
-DOCS_DIRECTORY = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
-
-
-def ensure_pandoc_installed(_):
-    import pypandoc
-
-    # Download pandoc if necessary. If pandoc is already installed and on
-    # the PATH, the installed version will be used. Otherwise, we will
-    # download a copy of pandoc into docs/bin/ and add that to our PATH.
-    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
-    # Add dir containing pandoc binary to the PATH environment variable
-    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
-        os.environ["PATH"] += os.pathsep + pandoc_dir
-    pypandoc.ensure_pandoc_installed(
-        targetfolder=pandoc_dir,
-        delete_installer=True,
-    )
-
-
-def setup(app):
-    app.connect("builder-inited", ensure_pandoc_installed)
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
