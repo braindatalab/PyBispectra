0b6a8b23ba242021b5d74b45b3e5136066bf1075
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-08-31 16:08:20 +0200

    test rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "numpydoc",
     "nbsphinx",
+    "nbsphinx_link",
     "sphinxcontrib.bibtex",
     "sphinx_gallery.gen_gallery",
 ]
@@ -48,30 +49,6 @@ sphinx_gallery_conf = {
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
-# Get path to directory containing this file, conf.py.
-DOCS_DIRECTORY = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
-
-
-def ensure_pandoc_installed(_):
-    import pypandoc
-
-    # Download pandoc if necessary. If pandoc is already installed and on
-    # the PATH, the installed version will be used. Otherwise, we will
-    # download a copy of pandoc into docs/bin/ and add that to our PATH.
-    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
-    # Add dir containing pandoc binary to the PATH environment variable
-    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
-        os.environ["PATH"] += os.pathsep + pandoc_dir
-    pypandoc.ensure_pandoc_installed(
-        targetfolder=pandoc_dir,
-        version="3.1.6.2",
-        delete_installer=True,
-    )
-
-
-def setup(app):
-    app.connect("builder-inited", ensure_pandoc_installed)
 
 
 # -- Options for HTML output -------------------------------------------------
