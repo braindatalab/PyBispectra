794ecf1709511e74817318ed054ffadad0be5e51
Thomas S. Binns <t.s.binns@outlook.com>
2024-09-18 19:17:58 +0200

    [MAINT] Prepare v1.2.0 release (#24)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,9 @@ import pybispectra
 from pybispectra.utils._docs import linkcode_resolve
 
 project = "PyBispectra"
-copyright = "2023, Thomas Samuel Binns"
-author = "Thomas Samuel Binns"
-release = "1.2.0dev"
+copyright = "2023, Thomas S. Binns"
+author = "Thomas S. Binns"
+release = "1.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/pybispectra/tde/__init__.py
+++ src/pybispectra/tde/__init__.py
@@ -1,5 +1,5 @@
 """Tools for handling time delay estimation analysis."""
 
-__version__ = "1.2.0dev"
+__version__ = "1.2.0"
 
 from .tde import TDE
