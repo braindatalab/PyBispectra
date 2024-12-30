2399be6e91b6b33eab28ed8d6db8767d8f257e8a
Thomas Samuel Binns <t.s.binns@outlook.com>
2024-12-30 14:35:16 +0000

    [MAINT] Post-release version update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ from pybispectra.utils._docs import linkcode_resolve
 project = "PyBispectra"
 copyright = "2023, Thomas S. Binns"
 author = "Thomas S. Binns"
-release = "1.2.1"
+release = "1.3.0dev"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
